99c30e9c0a88cafe97084ad94d8dda8933d78d3f
DmitryM80 <mihalych80@gmail.com>
2021-05-06 00:15:25 +0500

    Задача 4

--- tasks.js
+++ tasks.js
@@ -49,3 +49,21 @@ document.querySelector('#ts-3').addEventListener('click', function () {
     document.querySelector('#result-3-odd').innerText = 'Odds qty: '+ odds;
     document.querySelector('#result-3-even').innerText = 'Evens qty: '+ evens;
 });
+
+document.querySelector('#ts-4').addEventListener('click', function () {
+    const numsArr = [];
+    let numbsSum = 0;
+    let numbsAverage = 0;
+
+    for (let i=0; i<30; i++) {
+        let newNum = Math.floor(Math.random(1000) * 1000);
+        numsArr.push(newNum);
+        numbsSum += newNum;
+    }
+
+    numbsAverage = numbsSum / numsArr.length;
+
+    document.querySelector('#arr-4').innerText = 'The Array: '+numsArr;
+    document.querySelector('#result-4-sum').innerText = 'Numbers sum: '+ numbsSum;
+    document.querySelector('#result-4-average').innerText = 'Average: '+ numbsAverage;
+});
